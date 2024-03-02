4214fa0de8a605bf6b3b31f90f91dcc4fd0e6cad
Alfikri <83638938+Alfikriangelo@users.noreply.github.com>
2024-03-02 10:57:33 +0700

    nambahin fitur logout

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -5,8 +5,21 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLogoutClick = () => {
+    const isAuthenticated = true; 
+
+    if (isAuthenticated) {
+      navigate('/');
+    } else {
+      navigate('/home');
+    }
+  };
+
   return (
     <div>
       <AppBar sx={{ background: 'white' }}>
@@ -23,7 +36,7 @@ export default function Navbar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} style={{color: '#263237'}} >
             RT 05 / RW 24
           </Typography>
-          <Button style={{color: '#B80000', textTransform: 'none' }}>Keluar</Button>
+          <Button style={{color: '#B80000', textTransform: 'none' }} onClick={handleLogoutClick}>Keluar</Button>
         </Toolbar>
       </AppBar>
     </div>
